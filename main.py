5219ff57ed5659bb1a17933e27964e82449b7b2e
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-08-04 10:56:38 +0200

    Complete topic 2
    Variables

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-# This is my first python program
-print("Hello World!")
-print("It's good to be here!")
+# Strings
+first_name = "Filterd"
+food = "pide"
+email = "email@email.com"
+
+# Integers
+
+age = 20
+quantity = 3
+num_of_students = 30
+
+# Float
+
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# Boolean
+
+is_student = False
+for_sale = False
+is_online = True
+
+if is_online:
+    print("You are online")
+else:
+    print("You are offline")
