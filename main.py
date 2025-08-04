142e05153f83d30c26a0fdfaf964f2f68f53bca3
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-08-04 11:03:58 +0200

    Complete topic 3
    Type casting

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-# Strings
-first_name = "Filterd"
-food = "pide"
-email = "email@email.com"
-
-# Integers
-
+name = ""
 age = 20
-quantity = 3
-num_of_students = 30
-
-# Float
-
-price = 10.99
 gpa = 3.2
-distance = 5.5
+is_student = True
 
-# Boolean
+name = bool(name)
 
-is_student = False
-for_sale = False
-is_online = True
-
-if is_online:
-    print("You are online")
-else:
-    print("You are offline")
+print(name)
