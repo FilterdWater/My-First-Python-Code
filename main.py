e24b59c9faffd4f331e254e3a448429ed15cfd00
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-08-07 10:43:39 +0200

    Complete topic 8
    Calculator program

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-online = True
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2de number: "))
 
-if online:
-    print("The user is online")
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
 else:
-    print("The user is offline")
+    print(f"{operator} is not a valid operator")
