bdb7fa13d23a3c13eb352342256a8c961502985a
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-08-08 16:08:27 +0200

    Complete topic 13
    String methods

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-num = 5
-a = 6
-b = 7
-age = 13
-temperature = 20
-user_role = "guest"
+username = input("Enter a username: ")
 
-# print("Positive" if num > 0 else "Negative")
-# result = "EVEN" if num % 2 == 0 else "ODD"
-# max_num = a if a > b else b
-# min_num = a if a < b else b
-# status = "Adult" if age >= 18 else "Child"
-# weather = "HOT" if temperature > 20 else "COLD"
-access_level = "Full Access" if user_role == "admin" else "Limited Access"
-
-print(access_level)
+if len(username) > 12:
+    print("Your username can't be more than 12 characters")
+elif not username.find(" ") == -1:
+    print("Your username can't contain spaces")
+elif not username.isalpha():
+    print("Your username can't contain numbers")
+else:
+    print(f"Welcome {username}")
--- notes.md
+++ notes.md
@@ -9,3 +9,5 @@ A markdown file containing personal notes and experiences related to learning Py
 Don't even know anything about the Python ecosystem and what I would use the programming langauge for but seeing [Astral's](https://astral.sh/) tools gets me excited.
 
 Creating super simple stuff following Bro Code's course is a nice change of pace too.
+
+For some reason I needed a moment to be able to walk through the code on line 5 topic 13, so getting it was nice.
