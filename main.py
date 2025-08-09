912dae7dee99086e333e2283aa41f3671e68091c
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-08-09 07:33:57 +0200

    Complete topic 14
    String indexing

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-username = input("Enter a username: ")
+credit_number = "1234-5678-9012-3456"
 
-if len(username) > 12:
-    print("Your username can't be more than 12 characters")
-elif not username.find(" ") == -1:
-    print("Your username can't contain spaces")
-elif not username.isalpha():
-    print("Your username can't contain numbers")
-else:
-    print(f"Welcome {username}")
+credit_number = credit_number[::-1]
+print(credit_number)
