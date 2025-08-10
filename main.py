6d841a08eb5746d0708b141f56c10ca811353f5a
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-08-10 15:19:41 +0200

    Complete topc 18
    For loops

--- main.py
+++ main.py
@@ -1,29 +1,5 @@
-# Python compound interest calculator
-
-principle = 0
-rate = 0
-time = 0
-
-while True:
-    principle = float(input("Enter the principle amount: "))
-    if principle < 0:
-        print("principle can't be less than zero")
-    else:
+for x in range(1, 21):
+    if x == 13:
         break
-
-while True:
-    rate = float(input("Enter the interest rate: "))
-    if rate < 0:
-        print("Interest rate can't be less than zero")
     else:
-        break
-
-while True:
-    time = int(input("Enter the time in years: "))
-    if time < 0:
-        print("Time can't be less than zero")
-    else:
-        break
-
-total = principle * pow((1 + rate / 100), time)
-print(f"Balance after {time} year/s: €{total:.2f}")
+        print(x)
