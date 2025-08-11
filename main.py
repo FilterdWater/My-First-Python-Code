9301d35718d5f19251aa2cbe622f84b6bc6e5739
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-08-11 18:39:11 +0200

    Complete topic 22
    Shopping cart program

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-fruits = ("apple", "orange", "banana", "coconut", "coconut")
-# print(dir(fruits))
-# print(help(fruits))
-# print(len(fruits))
-# print("pineapple" in fruits)
+foods = []
+prices = []
+total = 0
 
-# print(fruits.index("apple"))
-print(fruits.count("coconut"))
+while True:
+    food = input("Enter a food to buy (q to quit): ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of the {food}: €"))
+        foods.append(food)
+        prices.append(price)
 
-# print(fruits)
-for fruit in fruits:
-    print(fruit)
+print("----- YOUR CART -----")
+
+for food in foods:
+    print(food, end=" ")
+
+for price in prices:
+    total += price
+
+print()
+print(f"Your total is: €{total}")
