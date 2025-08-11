d4b90054ee3aec19f221f14e605402d2bde4c9f1
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-08-11 18:59:00 +0200

    Improve topic 22
    - Add some error handeling
    - Show price per item

--- main.py
+++ main.py
@@ -6,15 +6,24 @@ while True:
     food = input("Enter a food to buy (q to quit): ")
     if food.lower() == "q":
         break
+    elif food == "":
+        print("You forgot to enter a food")
+        continue
     else:
-        price = float(input(f"Enter the price of the {food}: €"))
-        foods.append(food)
-        prices.append(price)
+        while True:
+            try:
+                price = float(input(f"Enter the price of the {food}: €"))
+            except ValueError:
+                print("Invalid input please enter a number")
+                continue
+            foods.append(food)
+            prices.append(price)
+            break
 
 print("----- YOUR CART -----")
 
 for food in foods:
-    print(food, end=" ")
+    print(food, end=f" €{price}\n")
 
 for price in prices:
     total += price
