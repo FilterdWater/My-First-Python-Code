87652514c5a6512308a4571d84e54188acc20375
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-08-13 09:59:57 +0200

    Complete topic 23
    2D collections

--- main.py
+++ main.py
@@ -1,32 +1,6 @@
-foods = []
-prices = []
-total = 0
+num_pad = ((1, 2, 3), (4, 5, 6), (7, 8, 9), ("*", 0, "#"))
 
-while True:
-    food = input("Enter a food to buy (q to quit): ")
-    if food.lower() == "q":
-        break
-    elif food == "":
-        print("You forgot to enter a food")
-        continue
-    else:
-        while True:
-            try:
-                price = float(input(f"Enter the price of the {food}: €"))
-            except ValueError:
-                print("Invalid input please enter a number")
-                continue
-            foods.append(food)
-            prices.append(price)
-            break
-
-print("----- YOUR CART -----")
-
-for food in foods:
-    print(food, end=f" €{price}\n")
-
-for price in prices:
-    total += price
-
-print()
-print(f"Your total is: €{total}")
+for row in num_pad:
+    for num in row:
+        print(num, end=" ")
+    print()
