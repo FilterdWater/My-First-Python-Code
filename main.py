f15ea44759f596962f419e6d26601bdc34ab9220
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-08-24 10:28:37 +0200

    Complete topic 31
    Functions

--- main.py
+++ main.py
@@ -1,56 +1,8 @@
-import random
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
 
-dice_art = {
-    1: ("┌─────────┐",
-        "│         │",
-        "│    ●    │",
-        "│         │",
-        "└─────────┘"),
-    2: ("┌─────────┐",
-        "│  ●      │",
-        "│         │",
-        "│      ●  │",
-        "└─────────┘"),
-    3: ("┌─────────┐",
-        "│  ●      │",
-        "│    ●    │",
-        "│      ●  │",
-        "└─────────┘"),
-    4: ("┌─────────┐",
-        "│  ●   ●  │",
-        "│         │",
-        "│  ●   ●  │",
-        "└─────────┘"),
-    5: ("┌─────────┐",
-        "│  ●   ●  │",
-        "│    ●    │",
-        "│  ●   ●  │",
-        "└─────────┘"),
-    6: ("┌─────────┐",
-        "│  ●   ●  │",
-        "│  ●   ●  │",
-        "│  ●   ●  │",
-        "└─────────┘")
-}
+full_name = create_name("spongebob", "squarepants")
 
-dice = []
-total = 0
-num_of_dice = int(input("How many dice?: "))
-
-for die in range(num_of_dice):
-    dice.append(random.randint(1, 6))
-
-# PRINT VERTICALLY
-# for die in range(num_of_dice):
-#     for line in dice_art.get(dice[die]):
-#         print(line)
-
-# PRINT HORIZONTALLY
-for line in range(5):
-    for die in dice:
-        print(dice_art.get(die)[line], end="")
-    print()
-
-for die in dice:
-    total += die
-print(f"total: {total}")
+print(full_name)
