c87849d59d4d418ce58cef256b35825e28e161ff
FilterdWater <129171301+FilterdWater@users.noreply.github.com>
2025-08-31 15:56:32 +0200

    Complete topic 39
    Modules

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-def is_weekend(day):
-    match day:
-        case "Saturday" | "Sunday":
-            return True
-        case _:
-            return False
+import example_module
 
+result1 = example_module.pi
+result2 = example_module.square(3)
+result3 = example_module.cube(3)
+result4 = example_module.circumference(3)
+result5 = example_module.area(3)
 
-print(is_weekend("Saturday"))
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
