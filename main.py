b5c4cedaf7220683a759de18753bf111fb43543d
filterdwater <filterdwater@fw-debian-t480.home>
2025-10-20 19:03:09 +0200

    Complete topic 44
    Encryption program

--- main.py
+++ main.py
@@ -1,91 +1,33 @@
 import random
+import string
 
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
 
-def spin_row():
-    symbols = ["🍒", "🍉", "🍋", "🔔", "⭐"]
+random.shuffle(key)
 
-    return [random.choice(symbols) for _ in range(3)]
-    # results = []
-    # for symbol in range(3):
-    #     results.append(random.choice(symbols))
-    # return results
+# print(f"chars: {chars}")
+# print(f"key  : {key}")
 
+# Encrypt
+plain_text = input("Enter a message to encrypt: ")
+cipher_text = ""
 
-def print_row(row):
-    print("-------------")
-    print(" | ".join(row))
-    print("-------------")
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
 
+print(f"Original message: {plain_text}")
+print(f"Encrypted message: {cipher_text}")
 
-def get_payout(row, bet):
-    if row[0] == row[1] == row[2]:
-        if row[0] == "🍒":
-            return bet * 5
-        elif row[0] == "🍉":
-            return bet * 5
-        elif row[0] == "🍋":
-            return bet * 5
-        elif row[0] == "🔔":
-            return bet * 10
-        elif row[0] == "⭐":
-            return bet * 20
-    return 0
+# Decrypt
+cipher_text = input("Enter a message to decrypt: ")
+plain_text = ""
 
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
 
-def main():
-    balance = 100
-
-    print("-----------------------")
-    print("Welcome to Python slots")
-    print("Symbols: 🍒🍉🍋🔔⭐ ")
-    print("-----------------------")
-
-    while balance > 0:
-        print("")
-        print(f"Current balance: €{balance}")
-
-        bet = input("Place your bet amount: ")
-
-        if not bet.isdigit():
-            print("Input invalid, try again")
-            continue
-
-        bet = int(bet)
-
-        if bet > balance:
-            print("Insufficient funds")
-            continue
-
-        if bet <= 0:
-            print("Bet must be greater than 0")
-            continue
-
-        balance -= bet
-
-        row = spin_row()
-        print("Spinning...\n")
-        print_row(row)
-
-        payout = get_payout(row, bet)
-        if payout > 0:
-            print("")
-            print(f"You won €{payout}")
-        else:
-            print("")
-            print("Sorry, you lost")
-
-        balance += payout
-
-        play_again = input("Play again? (y/N): ")
-
-        print("")
-        if play_again != "y":
-            break
-
-    print("------------------------------")
-    print(f"  Final balance: €{balance}  ")
-    print("------------------------------")
-
-
-if __name__ == "__main__":
-    main()
+print(f"Encrypted message: {cipher_text}")
+print(f"Original message: {plain_text}")
